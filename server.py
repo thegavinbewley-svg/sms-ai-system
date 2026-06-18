d4f1197a122608bf2ca482398e6d1b1bf2250923
Gavin Bewley <gavinbewley@Gavins-Laptop.local>
2026-06-18 03:56:43 -0700

    Update auth token

--- server.py
+++ server.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 # CONFIGURATION
 # ============================================================
 TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID", "ACadf86f354db213cd7b5769d3816f6c84")
-TWILIO_AUTH_TOKEN  = os.environ.get("TWILIO_AUTH_TOKEN", "bba89b16c02066e1cec0091ecbd100eb")
+TWILIO_AUTH_TOKEN  = os.environ.get("TWILIO_AUTH_TOKEN", "3ca7119907fa08da43fb5696bf5d035c")
 TWILIO_FROM_NUMBER = os.environ.get("TWILIO_FROM_NUMBER", "+12362432623")
 DATA_FILE          = "prospects.json"
 
